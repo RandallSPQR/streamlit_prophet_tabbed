559c8909884ff6a2b12728af7f018f9b89325dac
RandallSPQR <Randall.Bennington@gmail.com>
2022-11-17 16:55:39 -0500

    Again

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -62,7 +62,6 @@ with st.expander("What is this app?", expanded=False):
     st.write("")
 st.write("")
 st.sidebar.image(load_image("ExecuteMark_Dark_105px.png"), use_column_width=True)
-display_links(readme["links"]["repo"], readme["links"]["article"])
 
 
 st.sidebar.title("1. Data")
